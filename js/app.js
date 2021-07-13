4f7d0adc854e79787e59e7cc10da01e43bf72d85
Daniel Giustini Perez <danielgiustiniperez@gmail.com>
2021-07-13 17:17:17 +0200

    build skills section (content to be corrected)

--- js/app.js
+++ js/app.js
@@ -48,42 +48,57 @@ const states = {
     about: 0
 }
 
-for (i = 1; i <= document.querySelectorAll(".about__option").length; i++) {
-    dom.aboutOptions.push(new Element("about__option", i));
-    dom.aboutContent.push(new Element("about__content", i));
+const functions = {
+    fillArrays() {
+        for (i = 1; i <= document.querySelectorAll(".about__option").length; i++) {
+            dom.aboutOptions.push(new Element("about__option", i));
+            dom.aboutContent.push(new Element("about__content", i));
+        }
+    },
+    enableNavigation() {
+        dom.toggle.clickListen(function() {
+            const self = dom.toggle;
+            dom.container.toggleState("fixed");
+            self.toggleState();
+            self.disable(1000);
+            if (self.testState()) {
+                dom.nav.toggleState();
+            } else {
+                dom.nav.toggleState("exit", "active");
+                setTimeout(() => dom.nav.toggleState("exit"), 1000);
+            }
+        });
+    },
+    changeAbout(target, newIndex) {
+        const oldOption = dom.aboutOptions[states.about];
+        const oldContent = dom.aboutContent[states.about];
+        states.about = newIndex;
+        const newOption = target;
+        const newContent = dom.aboutContent[states.about];
+        oldOption.toggleState();
+        oldContent.toggleState("exit", "active");
+        newOption.toggleState();
+        newContent.toggleState();
+        setTimeout(() => oldContent.toggleState("exit"), 750);
+    },
+    enableAbout() {
+        dom.aboutOptions.forEach(option => option.clickListen(function() {
+            clearInterval(aboutInterval);
+            functions.changeAbout(option, option.index - 1);
+        }));
+    },
+    repeatAbout() {
+        return setInterval(() => {
+            let newIndex = states.about + 1;
+            if (newIndex >= dom.aboutOptions.length) {
+                newIndex = 0;
+            }
+            functions.changeAbout(dom.aboutOptions[newIndex], newIndex);
+        }, 7500);
+    }
 }
 
-dom.toggle.clickListen(function() {
-    const self = dom.toggle;
-    dom.container.toggleState("fixed");
-    self.toggleState();
-    self.disable(1000);
-    if (self.testState()) {
-        dom.nav.toggleState();
-    } else {
-        dom.nav.toggleState("exit", "active");
-        setTimeout(() => dom.nav.toggleState("exit"), 1000);
-    }
-});
-
-dom.aboutOptions.forEach(option => option.clickListen(function() {
-    const oldOption = dom.aboutOptions[states.about];
-    const oldContent = dom.aboutContent[states.about];
-    states.about = option.index - 1;
-    const newOption = option;
-    const newContent = dom.aboutContent[states.about];
-    oldOption.toggleState();
-    oldContent.toggleState("exit", "active");
-    newOption.toggleState();
-    newContent.toggleState();
-    setTimeout(() => oldContent.toggleState("exit"), 750);
-    // option.toggleState();
-    // dom.aboutContent[states.about].toggleState();
-
-}))
-
-// dom.options.forEach(o => o.addEventListener("click", () => {
-//     const currentOption = document.querySelector(".about__option--active");
-//     f.toggleState(currentOption, "about__option");
-//     f.toggleState(o, "about__option");
-// }));
+functions.fillArrays();
+functions.enableNavigation();
+const aboutInterval = functions.repeatAbout();
+functions.enableAbout();
